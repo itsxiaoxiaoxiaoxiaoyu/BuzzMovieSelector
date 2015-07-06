aedf1ce2a88828a5a8a9628f3aaca602401fce58
Dun Huang <huangdun@gatech.edu>
2015-07-05 22:51:01 -0400

    Added test file

---
+++ src/test/java/RegistrationControllerTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author huangdun
+ */
+public class RegistrationControllerTest {
+    @Before
+    public void setUp() {
+        RegistrationController registrationController = 
+                new RegistrationController();
+    }
+    
+    @Test
+    public void testSubmit() {
+        
+    }
+    
+}
